ecdc8fb6674363d8ab3cdd5e4dc1b6672a002173
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-07-27 22:36:38 +0800

    chore: cleanup components

--- apps/nativescript-app/src/home.component.ts
+++ apps/nativescript-app/src/home.component.ts
@@ -1,13 +1,13 @@
 import { NgFor } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA, Component, NO_ERRORS_SCHEMA, inject } from '@angular/core';
-import { RouterExtensions } from '@nativescript/angular';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, inject } from '@angular/core';
+import { NativeScriptCommonModule, RouterExtensions } from '@nativescript/angular';
 import { HlmButtonDirective } from '@spartan-ng/nativescript-ui-button-helm';
 
 @Component({
   selector: 'spartan-home',
   template: `
-    <ActionBar title="My App" class="action-bar" flat="true"> </ActionBar>
-    <StackLayout class="p-20">
+    <ActionBar title="Spartan App" class="action-bar" flat="true"> </ActionBar>
+    <StackLayout class="p-10">
       <Label text="Enjoy" class="h1 text-center c-white"></Label>
 
       <Button *ngFor="let route of routes" (tap)="navigate(route.link)" class="my-1" hlmBtn>
@@ -16,8 +16,8 @@ import { HlmButtonDirective } from '@spartan-ng/nativescript-ui-button-helm';
     </StackLayout>
   `,
   standalone: true,
-  imports: [NgFor, HlmButtonDirective],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  imports: [NativeScriptCommonModule, NgFor, HlmButtonDirective],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SpartanHomeComponent {
   private _routerExtensions = inject(RouterExtensions);
